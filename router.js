d84661ddfef865b8544e726430dab2959e18b3c0
nanyr <nanyramirez0@gmail.com>
2017-06-29 21:49:49 -0400

    fixed asynchronous requests

--- router.js
+++ router.js
@@ -4,16 +4,11 @@ const weather= require('./weather.js')
 const home=(req, res)=>{
     res.writeHead(200, {'Content-Type': 'text/plain'})
     if(req.url === '/'){
-      console.log('home')
       weather.get()
     res.write("done")
     res.end()
   }
 }
-
-// const user=(req, res)=>{
-//
-// }
 
 
 module.exports.home=home
--- weather.js
+++ weather.js
@@ -2,50 +2,60 @@
 const http= require('http')
 const key= require('./api.json')
 const request = require('request')
+const requestProm = require('request-promise');
 
-
-
-const print=(data)=>{
-  const info= `City: ${data.current_observation.display_location.city} \nState: ${data.current_observation.display_location.state} \n${data.current_observation.weather} \n${data.current_observation.temperature_string} `
-  console.log(info)
-}
 
 
 
 const getWeather= ()=>{
-  const zip;
-  request('http://ipinfo.io', function(error, res, body) {
-    const info=JSON.parse(body)
-    zip=info['postal']
+  requestProm('http://ipinfo.io')
+  .then((data)=> {
+    const info=JSON.parse(data)
+    return info
+      // return(info['postal'])
   })
+  .then((data)=>{requestProm(`http://api.wunderground.com/api/${key.key}/conditions/q/$sd.json`)
+  .then( (response)=>{
+    var res=JSON.parse(response)
+    if(res.response.error){
+      console.log(res.response.error.description)
+    }
+    // console.log(res.current_observation.display_location.full)
+    // console.log(res.current_observation.weather)
+    // console.log(res.current_observation.temperature_string)
 
-  try{
-    const request= http.get(`http://api.wunderground.com/api/${key.key}/conditions/q/${zip}.json`,  (response)=>{
-        if(response.statusCode ===200){
-          let body=""
-          response.on('data', (data)=>{
-            body+=data.toString()
-            })
-          response.on('end', ()=>{
-              const info= JSON.parse(body)
-              if(info.response.error){
-                const statusCodeError= new Error(`We couldn't find a location for zip code ${zip}, please try another location`)
-                console.error(statusCodeError.message)
-                }else{
-                  print(info)
-                }
-            })
-          response.on('error', ()=>{
-            console.error('There was a problem processing you request')
-          })
-        }else{
-          const statusCodeError= new Error(`There was a problem processing you request").(${http.STATUS_CODES[response.statusCode]}`)
-          console.error(statusCodeError)
-        }
-    })
-  }catch(e){
-    console.error(e.message)
-  }
+  })
+  .catch(function(err){
+    console.log(err)
+  })
+})
+  .catch(function(err){
+    console.log(err)
+  })
+// const requestPromise= http.get(`http://api.wunderground.com/api/${key.key}/conditions/q/${zip}.json`,  (response)=>{
+//         if(response.statusCode ===200){
+//           let body=""
+//           response.on('data', (data)=>{
+//             body+=data.toString()
+//             })
+//           response.on('end', ()=>{
+//               const info= JSON.parse(body)
+//               if(info.response.error){
+//                 const statusCodeError= new Error(`We couldn't find a location for zip code ${zip}, please try another location`)
+//                 console.error(statusCodeError.message)
+//                 }else{
+//                   print(info)
+//                 }
+//             })
+//           response.on('error', ()=>{
+//             console.error('There was a problem processing you request')
+//           })
+//         }else{
+//           const statusCodeError= new Error(`There was a problem processing you request").(${http.STATUS_CODES[response.statusCode]}`)
+//           console.error(statusCodeError)
+//         }
+//     })
+
 
 }
 
